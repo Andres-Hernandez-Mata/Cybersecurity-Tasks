534800c908557ba325833a66586490f80b29ff60
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-04-24 18:50:50 -0500

    Tiempo
    Version 1.1.0

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 """
 Uso: Menú principal
 Creador: Andrés Hernández Mata
-Version: 1.0.0
+Version: 1.1.0
 Python: 3.8.5
 Fecha: 05 Marzo 2021
 """
@@ -11,13 +11,14 @@ from colorama import Fore
 from colorama import Style
 import pyfiglet as header
 from termcolor import colored
+import time
+from datetime import datetime
 
 os.system("cls")
 
 def inicio():
     banner = header.figlet_format("Cybersecurity Tasks")
-    print(colored(banner.rstrip("\n"), "red", attrs=["bold"]))
-    print(colored("         By Andrés Hernández Mata | Versión 1.0.0 | LSTI \n", "yellow", attrs=["bold"]))
+    print(colored(banner.rstrip("\n"), "red", attrs=["bold"]))    
 
 def selecciona():
     try:        
@@ -38,7 +39,7 @@ def menu():
             print(colored("[03] Cifrado de Mensajes", "green", attrs=["bold"]))
             print(colored("[04] Envío de Correos", "green", attrs=["bold"]))
             print(colored("[05] Obtención de Metadatos", "green", attrs=["bold"]))
-            print(colored("[06] Salir", 'red', attrs=["bold"]))
+            print(colored("[06] Salir", 'green', attrs=["bold"]))
             opcion = selecciona()            
             if opcion == "1" or opcion == "01":
                 print(colored("Web Scraping", "red", attrs=["bold"]))
@@ -51,7 +52,9 @@ def menu():
             elif opcion == "5" or opcion == "05":
                 print(colored("Obtención de Metadatos", "red", attrs=["bold"]))
             elif opcion == "6" or opcion == "06":
-                print(colored("[INFO] Adios", "yellow", attrs=["bold"]))
+                tiempo = datetime.now()
+                print(colored(str(tiempo) + " [INFO] By Andrés Hernández Mata | Versión 1.1.0 | LSTI", "yellow", attrs=["bold"]))
+                print(colored(str(tiempo) + " [INFO] Gracias", "yellow", attrs=["bold"]))
                 break                
             else:
                 os.system("cls")
