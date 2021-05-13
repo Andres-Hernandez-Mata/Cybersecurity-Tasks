af69dcd48abdda65bf0496b9f64fb5d6478eb087
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-05-13 02:23:22 -0500

    Llamar funciones
    Version 2.0.0

--- src/cifrado/spanish.py
+++ src/cifrado/spanish.py
@@ -1,42 +1,41 @@
 """
 Uso: Spanish
-Creado: Andrés Hernández Mata
-Version: 1.1.0
+Creador: Andrés Hernández Mata
+Version: 2.0.0
 Python: 3.9.1
 Fecha: 11 Mayo 2020
 """
-
+# Krod Pxqgr
 class Spanish:
     
-    upper_letters = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZÁÉÍÓÚÄËÏÖÜ"
-    letters_and_space = upper_letters + upper_letters.lower() + ' \t\n'
-
-    def load_dictionary():    
+    def load_dictionary(self):    
         dictionary_file = open("cifrado\spanish.txt", encoding="utf8")    
         spanish_words = {}
         for word in dictionary_file.read().split("\n"):
             word = word.upper()
             spanish_words[word] = None
         dictionary_file.close()
-        return spanish_words
-
-    spanish_words = load_dictionary()
+        return spanish_words    
 
     def get_spanish_count(self, message):
-        message = message.upper()    
-        message = remove_non_letters(message)    
+        detect_spanish = Spanish()
+        spanish_words = detect_spanish.load_dictionary()
+        message = message.upper() 
+        message = detect_spanish.remove_non_letters(message)    
         possible_words = message.split()    
         
         if possible_words == []:
             return 0.0
 
-        matches = 0
+        matches = 0        
         for word in possible_words:        
             if word in spanish_words:
                 matches += 1
         return float(matches) / len(possible_words)
 
     def remove_non_letters(self, message):
+        upper_letters = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZÁÉÍÓÚÄËÏÖÜ"
+        letters_and_space = upper_letters + upper_letters.lower() + ' \t\n'
         letters_only = []
         for symbol in message:
             if symbol in letters_and_space:
@@ -44,8 +43,9 @@ class Spanish:
         return ''.join(letters_only)
 
     def spanish(self, message, word_percentage=20, letter_percentage=85): 
-        words_match = get_spanish_count(message) * 100 >= word_percentage    
-        num_letters = len(remove_non_letters(message))
+        detect_spanish = Spanish()
+        words_match = detect_spanish.get_spanish_count(message) * 100 >= word_percentage    
+        num_letters = len(detect_spanish.remove_non_letters(message))
         message_letters_percentage = float(num_letters) / len(message) * 100
         letters_match = message_letters_percentage >= letter_percentage
         return words_match and letters_match
