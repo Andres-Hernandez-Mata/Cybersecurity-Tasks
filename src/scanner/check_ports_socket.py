c9469c03f892c3626754861993df8286837f4566
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-05-16 01:17:47 -0500

    Clase puerto
    Escanear puerto

--- src/scanner/check_ports_socket.py
+++ src/scanner/check_ports_socket.py
@@ -1,30 +1,33 @@
 """
 Uso: Socket
 Creado: Andrés Hernández Mata
-Version: 1.1.0
+Version: 2.0.0
 Python: 3.9.1
-Fecha: 16 Abril 2020
+Fecha: 16 Abril 2021
 """
 
 import socket
-import sys
-import faker
+from termcolor import colored
+from datetime import datetime
 
-def checkPortsSocket(ip,portlist):
-    #print("IP",ip,type(ip)) #str    
-    try:
-        for port in portlist:
-            sock= socket.socket(socket.AF_INET,socket.SOCK_STREAM)#tecnología, protocolo
-            sock.settimeout(5)
-            result = sock.connect_ex((ip,port)) #
-            if result == 0:
-                print ("Puerto {}: \t Abierto".format(port))
-            else:
-                print ("Puerto {}: \t Cerrado".format(port))
+class Puerto:
+    
+    def check_ports_socket(self, ip, portlist):
+        try:
+            print("-------------------------------------------------------")
+            for port in portlist:
+                sock= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                sock.settimeout(5)
+                result = sock.connect_ex((ip, port))                
+                if result == 0:
+                    print(colored("{} [INFO] Puerto {}:\tAbierto".format(datetime.now(), port), "green", attrs=["bold"]))
+                else:
+                    print(colored("{} [INFO] Puerto {}:\tCerrado".format(datetime.now(), port), "green", attrs=["bold"]))
+            print("-------------------------------------------------------\n")
+        except socket.error as error:
+            print(colored("%s [ERROR] Ha ocurrido un error" % datetime.now(), "red", attrs=["bold"]))
+            print(colored("{}\n".format(error), "red", attrs=["bold"]))            
+        finally:
             sock.close()
-    except socket.error as error:
-        print (str(error))
-        print ("Error de conexion")
-        sys.exit()
 
 
