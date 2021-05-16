ce90589d4f67dc432e460fe4830b98753994b2e5
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-05-16 03:43:33 -0500

    Clase para extrarer metadata de PDF's
    Version: 1.0.0

--- src/metadata/extract_data_PDF.py
+++ src/metadata/extract_data_PDF.py
@@ -3,26 +3,33 @@ Uso: Metadata de PDF's
 Creador: Andrés Hernández Mata
 Version: 1.0.0
 Python: 3.9.1
-Fecha: 26 Abril 2020
+Fecha: 26 Abril 2021
 """
 
-from PyPDF2 import PdfFileReader, PdfFileWriter
-import os 
+import os
+from PyPDF2 import PdfFileReader
+from termcolor import colored
+from datetime import datetime
 
-def printMeta():
-    ruta = input("Ruta de pdf: ")
-    os.chdir(ruta)
-    for root, dirs, files in os.walk(".", topdown=False):
-        for name in files:
-            ext = name.lower().rsplit('.', 1)[-1] #archivo.nombre.algo.pdf
-            print(name.lower().rsplit('.', 1))
-            input()
-            if ext in ['pdf']:
-                print("[+] Metadata for file: %s " %(ruta+os.path.sep+name))
-                pdfFile = PdfFileReader(open(ruta+os.path.sep+name, 'rb'))
-                docInfo = pdfFile.getDocumentInfo()
-                print("Tipo: ", type(docInfo)) 
-                for metaItem in docInfo:
-                    print('[+] ' + metaItem + ':' + docInfo[metaItem])
-                print("\n")
-printMeta()
+class PDF:
+
+    def get_metadata_pdf():
+        try:
+            ruta = input("Ruta de pdf's: ")
+            os.chdir(ruta)
+            for root, dirs, files in os.walk(".", topdown=False):
+                for name in files:
+                    ext = name.lower().rsplit(".", 1)[-1]
+                    print(name.lower().rsplit(".", 1))                    
+                    if ext in ["pdf"]:
+                        print("[+] Metadata for file: %s " % (ruta+os.path.sep+name))
+                        pdfFile = PdfFileReader(open(ruta+os.path.sep+name, "rb"))
+                        docInfo = pdfFile.getDocumentInfo()
+                        print("Tipo: ", type(docInfo))
+                        for metaItem in docInfo:
+                            print("[+] " + metaItem + ":" + docInfo[metaItem])
+                        print("\n")
+        
+        except Exception as error:
+            print(colored("%s [ERROR] Ha ocurrido un error" % datetime.now(), "red", attrs=["bold"]))
+            print(colored("{}\n".format(error), "red", attrs=["bold"]))
