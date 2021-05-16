6e0d9911abee16740c149a1afb3c43248b2ca1bb
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-05-16 03:34:59 -0500

    Clase para metadata de imagenes
    Version: 2.0.0

--- src/metadata/extract_data_images.py
+++ src/metadata/extract_data_images.py
@@ -1,73 +1,93 @@
 """
 Uso: Metadata de imagenes
 Creador: Andrés Hernández Mata
-Version: 1.0.0
+Version: 2.0.0
 Python: 3.9.1
-Fecha: 10 Mayo 2020
+Fecha: 10 Mayo 2021
 """
 
+import os
 from PIL.ExifTags import TAGS, GPSTAGS
 from PIL import Image
-import os
+from termcolor import colored
+from datetime import datetime
 
-def decode_gps_info(exif):
-    gpsinfo = {}
-    if 'GPSInfo' in exif:
-        #Parse geo references.
-        Nsec = exif['GPSInfo'][2][2] 
-        Nmin = exif['GPSInfo'][2][1]
-        Ndeg = exif['GPSInfo'][2][0]
-        Wsec = exif['GPSInfo'][4][2]
-        Wmin = exif['GPSInfo'][4][1]
-        Wdeg = exif['GPSInfo'][4][0]
-        if exif['GPSInfo'][1] == 'N':
-            Nmult = 1
-        else:
-            Nmult = -1
-        if exif['GPSInfo'][3] == 'E':
-            Wmult = 1
-        else:
-            Wmult = -1
-        Lat = Nmult * (Ndeg + (Nmin + Nsec/60.0)/60.0)
-        Lng = Wmult * (Wdeg + (Wmin + Wsec/60.0)/60.0)
-        exif['GPSInfo'] = {"Lat" : Lat, "Lng" : Lng}
+class Imagenes:
 
- 
-def get_exif_metadata(image_path):
-    ret = {}
-    image = Image.open(image_path)
-    if hasattr(image, '_getexif'):
-        exifinfo = image._getexif()
-        if exifinfo is not None:
-            for tag, value in exifinfo.items():
-                decoded = TAGS.get(tag, tag)
-                ret[decoded] = value
-    decode_gps_info(ret)
-    return ret
+    def decode_gps_info(self, exif):       
+        try:
+            if "GPSInfo" in exif:
+                Nsec = exif["GPSInfo"][2][2] 
+                Nmin = exif["GPSInfo"][2][1]
+                Ndeg = exif["GPSInfo"][2][0]
+                Wsec = exif["GPSInfo"][4][2]
+                Wmin = exif["GPSInfo"][4][1]
+                Wdeg = exif["GPSInfo"][4][0]
+                if exif["GPSInfo"][1] == "N":
+                    Nmult = 1
+                else:
+                    Nmult = -1
+                if exif["GPSInfo"][3] == "E":
+                    Wmult = 1
+                else:
+                    Wmult = -1
+                Lat = Nmult * (Ndeg + (Nmin + Nsec/60.0)/60.0)
+                Lng = Wmult * (Wdeg + (Wmin + Wsec/60.0)/60.0)
+                exif["GPSInfo"] = {"Lat" : Lat, "Lng" : Lng}
+        
+        except Exception as error:
+            print(colored("%s [ERROR] Ha ocurrido un error" % datetime.now(), "red", attrs=["bold"]))
+            print(colored("{}\n".format(error), "red", attrs=["bold"]))
     
-def printMeta():
-    file = open("data\extractData.txt", "w")
-    ruta = input("Ruta de imágenes: ")
-    os.chdir(ruta)
-    for root, dirs, files in os.walk(".", topdown=False):
-        for name in files:
-            file.write(os.path.join(root, name))
-            file.write(os.linesep)
-            file.write("[+] Metadata for file: %s " %(name))
-            file.write(os.linesep)
-            #input()
-            try:
-                exifData = {}
-                exif = get_exif_metadata(name)
-                for metadata in exif:
-                    file.write("Metadata: %s - Value: %s " %(metadata, exif[metadata]))
-                    file.write(os.linesep)                
-            except:
-                import sys, traceback
-                traceback.print_exc(file=sys.stdout)
-    file.close()
-    print("Puedes consultar el resultado obtenido en data\extractData.txt")
+    def get_exif_metadata(self, image_path):
+        try:
+            imagenes = Imagenes()
+            ret = {}
+            image = Image.open(image_path)
+            if hasattr(image, "_getexif"):
+                exif_info = image._getexif()
+                if exif_info is not None:
+                    for tag, value in exif_info.items():
+                        decoded = TAGS.get(tag, tag)
+                        ret[decoded] = value
+            imagenes.decode_gps_info(ret)            
+            return ret
+        
+        except Exception as error:
+            print(colored("%s [ERROR] Ha ocurrido un error" % datetime.now(), "red", attrs=["bold"]))
+            print(colored("{}\n".format(error), "red", attrs=["bold"]))
 
-printMeta()
+    def get_metadata_imagenes():
+        try:
+            imagenes = Imagenes()
+            file = open("extract_data_imagenes.txt", "w")
+            while True:
+                ruta = input("\nRuta de imágenes: ")
+                if not ruta:
+                    print(colored("\n%s [INFO] La ruta de imágenes es un dato obligatorio" % datetime.now(), "red", attrs=["bold"]))
+                else:
+                    break
+            os.chdir(ruta)
+            for root, dirs, files in os.walk(".", topdown=False):
+                for name in files:
+                    file.write(os.path.join(root, name))
+                    file.write(os.linesep)
+                    file.write("[+] Metadata for file: %s \n" % (name))                    
+                    try:
+                        exifData = {}
+                        exif = imagenes.get_exif_metadata(name)
+                        for metadata in exif:
+                            file.write("Metadata: %s - Value: %s \n" %(metadata, exif[metadata]))
+                    except:
+                        import sys, traceback
+                        traceback.print_exc(file=sys.stdout)            
+            print(colored("%s [INFO] Puedes consultar el resultado obtenido en extract_data_imagenes.txt" % datetime.now(), "green", attrs=["bold"]))
+        
+        except Exception as error:
+            print(colored("%s [ERROR] Ha ocurrido un error" % datetime.now(), "red", attrs=["bold"]))
+            print(colored("{}\n".format(error), "red", attrs=["bold"]))
+        finally:
+            file.close()
+
 
 
