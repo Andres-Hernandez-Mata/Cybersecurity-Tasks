0f8a4595133972ee02d1a004c0432a43fb60804a
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-05-17 19:40:10 -0500

    Clase beautiful soup
    Nueva clase

---
+++ src/scraping/beautiful_soup.py
@@ -0,0 +1,57 @@
+"""
+Uso: Web scraping
+Creador: Andrés Hernández Mata
+Version: 1.0.0
+Python: 3.9.1
+Fecha: 10 Mayo 2020
+"""
+
+import os
+import pyfiglet as header
+from datetime import datetime
+from termcolor import colored
+from scraping.scraping import Scraping
+
+clear = lambda: os.system("cls" if os.name=="nt" else "clear")
+
+class Beautiful:
+
+	def option(self):
+		try:
+			opcion = 0
+			while True:
+				opcion = input("[**] Elige una opción > ")
+				if not opcion:
+					print(colored("\n%s [INFO] Seleccionar una opción del menú" % datetime.now(), "red", attrs=["bold"]))
+				else:
+					break
+		
+		except Exception as error:
+			print(colored("%s [ERROR] Ha ocurrido un error" % datetime.now(), "red", attrs=["bold"]))
+			print(colored("%s [ERROR] Introduce un numero entero" % datetime.now(), "red", attrs=["bold"]))
+			print(colored("{}\n".format(error), "red", attrs=["bold"]))
+	
+		return opcion
+
+	def menu(self):
+		clear()
+		banner = header.figlet_format("Web Scraping")
+		print(colored(banner.rstrip("\n"), "red", attrs=["bold"]))
+		opcion = 0
+		try:
+			while True:
+				print(colored("[01] Beautiful Soup", "green", attrs=["bold"]))
+				print(colored("[02] Salir", "green", attrs=["bold"]))
+				beautiful = Beautiful()
+				scraping = Scraping()
+				opcion = beautiful.option()
+				if opcion == "1" or opcion == "01":					
+					scraping.url()
+				elif opcion == "2" or opcion == "02":
+					clear()
+					break
+				else:
+					print(colored("\n%s [INFO] Introduce una opción valida del menú" % datetime.now(), "red", attrs=["bold"]))
+		except Exception as error:
+			print(colored("%s [ERROR] Ha ocurrido un error" % datetime.now(), "red", attrs=["bold"]))
+			print(colored(error, "red", attrs=["bold"]))
