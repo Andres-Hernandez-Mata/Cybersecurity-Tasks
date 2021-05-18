dbc4fb4d9405795e1439b85ab9686a28588e013a
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-05-17 19:43:49 -0500

    Menu de metadata
    Version: 1.4.0

--- src/metadata/metadata.py
+++ src/metadata/metadata.py
@@ -1,7 +1,7 @@
 """
 Uso: Menu de metadata
 Creador: Andrés Hernández Mata
-Version: 1.0.0
+Version: 1.4.0
 Python: 3.9.1
 Fecha: 16 Mayo 2021
 """
@@ -11,51 +11,52 @@ import pyfiglet as header
 from termcolor import colored
 from datetime import datetime
 from metadata.extract_data_images import Imagenes
-from metadata.extract_data_pdf import PDF
+from metadata.extract_data_pdf import Documentos
 
 clear = lambda: os.system("cls" if os.name=="nt" else "clear")
 
 class Metadata:
 
     def option(self):
-        try:
-            opcion = 0
+        try:            
             while True:
                 opcion = input("[**] Elige una opción > ")
-                if opcion:                
-                    break
-                print(colored("\n%s [INFO] Seleccionar una opción del menú" % datetime.now(), "red", attrs=["bold"]))
+                if not opcion:
+                    print(colored("\n%s [INFO] Seleccionar una opción del menú" % datetime.now(), "red", attrs=["bold"]))
+                else:
+                    break                
         
         except Exception as error:
             print(colored("%s [ERROR] Ha ocurrido un error" % datetime.now(), "red", attrs=["bold"]))   
             print(colored("{}\n".format(error), "red", attrs=["bold"]))
         
-        return opcion          
+        return opcion
 
     def menu(self):
         clear()
         banner = header.figlet_format("Metadata")
-        print(colored(banner.rstrip("\n"), "red", attrs=["bold"]))
-        opcion = 0
+        print(colored(banner.rstrip("\n"), "red", attrs=["bold"]))        
         try:
-            while True:
+            while True:                
                 print(colored("[01] Extraer metadata de imagenes", "green", attrs=["bold"]))
                 print(colored("[02] Extraer metadata de PDF's", "green", attrs=["bold"]))
                 print(colored("[03] Salir", "green", attrs=["bold"]))
+                opcion = str()
                 metadata = Metadata()
+                imagenes = Imagenes()
+                documentos = Documentos()
                 opcion = metadata.option()
-                if opcion == "1" or opcion == "01":
-                    imagenes = Imagenes()
+                if opcion == "1" or opcion == "01":                    
                     imagenes.get_metadata_imagenes()
-                if opcion == "2" or opcion == "03":
-                    pdf = PDF()
-                    pdf.get_metadata_pdf()
-                elif opcion == "3" or opcion == "02":
+                elif opcion == "2" or opcion == "02":                    
+                    documentos.get_metadata_pdf()
+                elif opcion == "3" or opcion == "03":
                     clear()
                     break
-                else:                    
-                    print(colored("\n%s [INFO] Introduce una opcion valida del menú" % datetime.now(), "red", attrs=["bold"]))
+                else:                  
+                    print(colored("\n%s [INFO] Introduce una opción valida del menú" % datetime.now(), "red", attrs=["bold"]))
         
         except Exception as error:
-            print(colored("%s [ERROR] Ha ocurrido un error" % datetime.now(), "red", attrs=["bold"]))
+            print(colored("\n%s [ERROR] Ha ocurrido un error" % datetime.now(), "red", attrs=["bold"]))
             print(colored(error, "red", attrs=["bold"]))
+
