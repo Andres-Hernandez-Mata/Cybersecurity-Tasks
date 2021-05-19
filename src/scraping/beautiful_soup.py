911f34682c8364a1ef6247913d7fe3a8893bc1df
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-05-19 10:55:18 -0500

    Uso de logging en beautiful
    Version: 1.5.0

--- src/scraping/beautiful_soup.py
+++ src/scraping/beautiful_soup.py
@@ -1,18 +1,20 @@
 """
 Uso: Web scraping
 Creador: Andrés Hernández Mata
-Version: 1.0.0
+Version: 1.5.0
 Python: 3.9.1
 Fecha: 10 Mayo 2020
 """
 
 import os
 import pyfiglet as header
+import logging
 from datetime import datetime
 from termcolor import colored
 from scraping.scraping import Scraping
 
 clear = lambda: os.system("cls" if os.name=="nt" else "clear")
+logging.basicConfig(level=logging.INFO, filename="cybersecurity_tasks.log", format="%(asctime)s %(levelname)s:%(message)s")
 
 class Beautiful:
 
@@ -27,6 +29,7 @@ class Beautiful:
 					break
 		
 		except Exception as error:
+			logging.error(error, exc_info=True)
 			print(colored("%s [ERROR] Ha ocurrido un error" % datetime.now(), "red", attrs=["bold"]))
 			print(colored("%s [ERROR] Introduce un numero entero" % datetime.now(), "red", attrs=["bold"]))
 			print(colored("{}\n".format(error), "red", attrs=["bold"]))
@@ -53,5 +56,6 @@ class Beautiful:
 				else:
 					print(colored("\n%s [INFO] Introduce una opción valida del menú" % datetime.now(), "red", attrs=["bold"]))
 		except Exception as error:
+			logging.error(error, exc_info=True)
 			print(colored("%s [ERROR] Ha ocurrido un error" % datetime.now(), "red", attrs=["bold"]))
 			print(colored(error, "red", attrs=["bold"]))
