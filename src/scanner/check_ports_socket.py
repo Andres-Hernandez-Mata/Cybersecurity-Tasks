5f347d2768d2c3ce62e151009b499f9c5eae4ab9
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-05-19 10:57:04 -0500

    Uso de logging en puerto
    Version: 2.5.0

--- src/scanner/check_ports_socket.py
+++ src/scanner/check_ports_socket.py
@@ -1,15 +1,17 @@
 """
 Uso: Socket
 Creado: Andrés Hernández Mata
-Version: 2.0.0
+Version: 2.5.0
 Python: 3.9.1
 Fecha: 16 Abril 2021
 """
 
 import socket
+import logging
 from termcolor import colored
 from datetime import datetime
 
+logging.basicConfig(level=logging.INFO, filename="cybersecurity_tasks.log", format="%(asctime)s %(levelname)s:%(message)s")
 class Puerto:
     
     def check_ports_socket(self, ip, portlist):
@@ -25,6 +27,7 @@ class Puerto:
                     print(colored("{} [INFO] Puerto {}:\tCerrado".format(datetime.now(), port), "green", attrs=["bold"]))
             print("-------------------------------------------------------\n")
         except socket.error as error:
+            logging.error(error, exc_info=True)
             print(colored("%s [ERROR] Ha ocurrido un error" % datetime.now(), "red", attrs=["bold"]))
             print(colored("{}\n".format(error), "red", attrs=["bold"]))            
         finally:
