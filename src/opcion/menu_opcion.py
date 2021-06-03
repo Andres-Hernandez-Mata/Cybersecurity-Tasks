20b174458b33e19919022e43d4789b1d00ecabf5
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-06-02 21:37:24 -0500

    Funcion de opcion del menu

--- src/opcion/menu_opcion.py
+++ src/opcion/menu_opcion.py
@@ -6,5 +6,27 @@ Python: 3.9.1
 Fecha: 02 Junio 2021
 """
 
+import logging
+from termcolor import colored
+from datetime import datetime
+
+logging.basicConfig(level=logging.INFO, filename="cybersecurity_tasks.log", format="%(asctime)s %(levelname)s:%(message)s")
+
 class Opcion:
-    pass
+    
+    def opcion(self):
+        try:
+            i = True
+            while(i == True):
+                opcion = input("[**] Elige una opción > ")
+                if not opcion:
+                    print(colored("{} [INFO] Seleccionar una opción del menú \n".format(datetime.now()), "red", attrs=["bold"]))
+                else:
+                    i = False                                    
+
+        except Exception as error:
+            logging.error(error, exc_info=True)
+            print(colored("%s [ERROR] Ha ocurrido un error" % datetime.now(), "red", attrs=["bold"]))
+            print(colored("{}\n".format(error), "red", attrs=["bold"]))
+
+        return opcion
