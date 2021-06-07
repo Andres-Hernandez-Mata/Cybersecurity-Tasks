329e1845f0e1798cb22b7e4d32e549863b1dc60c
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-06-06 21:45:14 -0500

    Importar modulo de opcion

--- src/scapydot11/scapy_dot11.py
+++ src/scapydot11/scapy_dot11.py
@@ -12,9 +12,12 @@ import pyfiglet as header
 from datetime import datetime
 from termcolor import colored
 from scapydot11.network import Network
+from opcion.menu_opcion import Opcion
 
 clear = lambda: os.system("cls" if os.name=="nt" else "clear")
 logging.basicConfig(level=logging.INFO, filename="cybersecurity_tasks.log", format="%(asctime)s %(levelname)s:%(message)s")
+opcion = Opcion()
+global opcion_menu
 
 class ScapyDot11:
 
