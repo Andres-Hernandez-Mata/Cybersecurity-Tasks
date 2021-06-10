7d5b872dd88cec414f1d93e3166bb03bc0d46ae0
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-06-10 00:01:10 -0500

    Eliminar funcion de escanear

--- src/scanner/scanner_puertos.py
+++ src/scanner/scanner_puertos.py
@@ -11,40 +11,18 @@ import pyfiglet as header
 import logging
 from termcolor import colored
 from datetime import datetime
-from scanner.check_ports_socket import Puerto
 from scanner.scanner_nmap import ScannerNmap
+from scanner.escanear import Escanear
 from opcion.menu_opcion import Opcion
 
 clear = lambda: os.system("cls" if os.name=="nt" else "clear")
 logging.basicConfig(level=logging.INFO, filename="cybersecurity_tasks.log", format="%(asctime)s %(levelname)s:%(message)s")
 scanner_nmap = ScannerNmap()
+escanear = Escanear()
 opcion = Opcion()
 global opcion_menu
 
 class ScannerPuertos:
-
-    def escanear(self):
-        try:
-            while True:
-                ip = input("\nTarget > ")
-                port_list = input("Puertos > ")
-                if not ip or not port_list:
-                    print(colored("%s [INFO] El target y el puerto son datos obligatorios" % datetime.now(), "red", attrs=["bold"]))
-                else:
-                    break
-            
-            port_list = port_list.split(",")
-    
-            for i in range(len(port_list)):   
-                port_list[i] = int(port_list[i])
-    
-            puerto = Puerto()
-            puerto.check_ports_socket(ip, port_list)
-        
-        except Exception as error:
-            logging.error(error, exc_info=True)
-            print(colored("%s [ERROR] Ha ocurrido un error" % datetime.now(), "red", attrs=["bold"]))
-            print(colored("{}\n".format(error), "red", attrs=["bold"]))
 
     def menu(self):
         clear()
@@ -54,11 +32,10 @@ class ScannerPuertos:
             while True:
                 print(colored("[01] Socket", "green", attrs=["bold"]))
                 print(colored("[02] Nmap", "green", attrs=["bold"]))
-                print(colored("[03] Salir", "green", attrs=["bold"]))                
-                escaneo = Escaneo()
+                print(colored("[03] Salir", "green", attrs=["bold"]))
                 opcion_menu = opcion.opcion()      
                 if opcion_menu == "1" or opcion_menu == "01":
-                    escaneo.escanear()
+                    escanear.escanear()
                 elif opcion_menu == "2" or opcion_menu == "02":                    
                     scanner_nmap.scanner_nmap()
                 elif opcion_menu == "3" or opcion_menu == "03":
